fb868b090617e0d2d7e6f2a5c633e9911029b36c
ZhaoGuan <guanzhaogz@gmail.com>
2020-11-26 20:47:38 +0800

    update req

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 rm -rf build dist
-pyinstaller ./main.py -n android_performance \
-  --hidden-import=yaml \
-  --hidden-import=jinja2
+#pyinstaller -F ./main.py -n android_performance \
+#  --hidden-import=yaml \
+#  --hidden-import=jinja2
+pyinstaller ./main.py -n android_performance
 cp -rf moudle/mitmproxy ./dist/android_performance
 cp -rf proxy ./dist/android_performance
